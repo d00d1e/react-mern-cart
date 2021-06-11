e28f82c6f4eaae3329797e0969d849cc97d3cf84
d00d1e <lee.julianay@gmail.com>
2021-06-11 14:47:25 -0700

    add ProductView and Product components

--- frontend/src/views/HomeView/HomeView.js
+++ frontend/src/views/HomeView/HomeView.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Product from "../../components/Product/Product";
 import "./HomeView.css";
 
 export default function HomeView() {
   return (
     <div className="homeview">
-      <p>Home View</p>
+      <h1 className="homeview__title">Latest Products</h1>
+      <div className="homeview__products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   );
 }
