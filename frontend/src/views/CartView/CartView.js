854437af8f1b80ed82827788f76e6372aef47e61
d00d1e <lee.julianay@gmail.com>
2021-06-12 23:42:25 -0700

    add/style cartView and cartItems

--- frontend/src/views/CartView/CartView.js
+++ frontend/src/views/CartView/CartView.js
@@ -1,10 +1,23 @@
 import React from "react";
+import CartItem from "../../components/CartItem/CartItem";
 import "./CartView.css";
 
 export default function CartView() {
   return (
     <div className="cartview">
-      <p>Cart View</p>
+      <div className="cartview__left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+      </div>
+      <div className="cartview__right">
+        <div className="cartview__info">
+          <p>Subtotal: (0) items</p>
+          <p>$499.99</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+      </div>
     </div>
   );
 }
